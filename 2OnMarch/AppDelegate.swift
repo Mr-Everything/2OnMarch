66b42c38a4356fbc070f64cf39abacb9a42d1ab2
ahmedsamir <ahmed.sam.20019@gmail.com>
2019-03-10 14:15:12 +0200

    ahmed push

--- 2OnMarch/AppDelegate.swift
+++ 2OnMarch/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         IQKeyboardManager.shared().isEnabled = true 
         UIApplication.shared.statusBarView?.backgroundColor = Theme.NavigationColor
         
+        
         return true
     }
 
